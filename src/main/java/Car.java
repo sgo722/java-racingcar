a794621722eb74326908c15c41f3db62ac3c5a14
sgo722 <sgo722@naver.com>
2025-03-24 02:04:21 +0900

    [FEAT] 자동차 객체 구현

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,5 +1,26 @@
+import java.util.Random;
+
 public class Car {
 
     // 이름을 가진다.
     // 조건에 따라 전진한다.
+
+    private String name;
+    private Long moveCount;
+
+    public Car(String name) {
+        this.name = name;
+        this.moveCount = 0L;
+    }
+
+    public void move(){
+        if (isPossible()) {
+            moveCount++;
+        }
+    }
+
+    public boolean isPossible() {
+        Random rand = new Random();
+        return rand.nextInt(9) >= 4;
+    }
 }
